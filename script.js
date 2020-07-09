bf2fca62961f1e4bf1e63f1b0043cbd5199c7e01
Glitch (personality-quiz-starter-code) <none>
2020-07-09 18:58:17 +0000

    💽🌾 Checkpoint ./script.js:18579135/10011

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ var sourdoughcount = 0;
 
 //#TODO: Use the Document Object Model (DOM) to create variables for the first quiz question.
 
+var result = document.getElementById("result");
+
 //Q1
 var q1a1 = document.getElementById("q1a1");
 var q1a2 = document.getElementById("q1a2");
@@ -34,30 +36,69 @@ q1a2.addEventListener("click", focaccia);
 q1a3.addEventListener("click", sourdough);
 q1a4.addEventListener("click", pan);
 
-q1a1.addEventListener("click", baguette);
-q1a2.addEventListener("click", focaccia);
-q1a3.addEventListener("click", sourdough);
-q1a4.addEventListener("click", pan);
+q2a1.addEventListener("click", baguette);
+q2a2.addEventListener("click", focaccia);
+q2a3.addEventListener("click", sourdough);
+q2a4.addEventListener("click", pan);
 
+q3a1.addEventListener("click", baguette);
+q3a2.addEventListener("click", focaccia);
+q3a3.addEventListener("click", sourdough);
+q3a4.addEventListener("click", pan);
 
 //#TODO: Define quiz functions here
 
 function baguette() {
   baguettecount = baguettecount + 1; //Track # of baguette answers
   questioncount = questioncount + 1; //Track # quiz answers
+  alert("Response Confirmed");
+  if (questioncount >= 3) {
+    updateresult();
+  }
+ 
 }
 
 function focaccia() {
   focacciacount = focacciacount + 1;
   questioncount = questioncount + 1;
+  if (questioncount >= 3) {
+    updateresult();
+  }
+  alert("Response Confirmed");
 }
 
 function pan() {
   pancount = pancount + 1;
   questioncount = questioncount + 1;
+  if (questioncount >= 3) {
+    updateresult();
+  }
+  alert("Response Confirmed");
 }
 
 function sourdough() {
   sourdoughcount = sourdoughcount + 1;
   questioncount = questioncount + 1;
+  if (questioncount >= 3) {
+    updateresult();
+  }
+  alert("Response Confirmed");
+}
+
+function update result (){
+if (baguettecount >= 2){
+result.innerHTML = "You are a... Baguette!"
+ }
+ else if(focacciacount>=2){
+result.innerHTML = "You are... Focaccia!"
+ }
+ else if(sourdoughcount>=2){
+result.innerHTML = "You are... Sourdough!"
+ }
+ else if (pancount>= 2){
+ result.innerHTML = "You are... Pan de Sal!"
+ }
+ else{
+  result.innerHTML = "You are... The (B)avatar! Master of all four (b)elements"
+ }
 }
