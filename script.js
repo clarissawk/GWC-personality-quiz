e2b0beac149bccba5ab27aae23651d9e969ea157
Glitch (personality-quiz-starter-code) <none>
2020-07-09 18:30:54 +0000

    🌶🖐 Checkpoint ./script.js:18579135/2634 ./index.html:18579135/68625

--- script.js
+++ script.js
@@ -1,27 +1,18 @@
 //Global Variables
 //This is where you will define the variables you will be using in your project.
 //#TODO: Create four variables to track each possible quiz outcome
- 
- 
+
 var questioncount = 0;
 var baguettecount = 0;
 var focacciacount = 0;
 var pancount = 0;
 var sourdoughcount = 0;
 
-
-
-
-
-
-
 //#TODO: Use the DOM to create variables for the first quiz question.
 var q1a1 = document.getElementById("q1a1");
-
-
-
-
-
+var q1a2 = document.getElementById("q1a2");
+var q1a3 = document.getElementById("q1a3");
+var q1a4 = document.getElementById("q1a4");
 
 //#TODO: Add Event Listeners to your answer choice variables.
 q1a1.addEventListener("click", baguette);
@@ -29,30 +20,24 @@ q1a2.addEventListener("click", focaccia);
 q1a3.addEventListener("click", pan);
 q1a4.addEventListener("click", sourdough);
 
-
-
-
-
-
-
 //#TODO: Define quiz functions here
 
-function baguette () {
-  baguettecount = baguettecount + 1;   //# Track # of baguette answers 
-  questioncount = questioncount + 1;   //# Track # quiz answers 
+function baguette() {
+  baguettecount = baguettecount + 1; //# Track # of baguette answers
+  questioncount = questioncount + 1; //# Track # quiz answers
 }
 
-function focaccia () {
+function focaccia() {
   focacciacount = focacciacount + 1;
-  questioncount = questioncount + 1; 
+  questioncount = questioncount + 1;
 }
 
-function pan () {
+function pan() {
   pancount = pancount + 1;
-  questioncount = questioncount + 1; 
+  questioncount = questioncount + 1;
 }
 
-function sourdough () {
+function sourdough() {
   sourdoughcount = sourdoughcount + 1;
-  questioncount = questioncount + 1; 
+  questioncount = questioncount + 1;
 }
